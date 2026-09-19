b09c40ed37873f0a572735173ac2a639f2899e98
Ferdinand Ganter <100222172+ganfer@users.noreply.github.com>
2026-09-19 04:49:06 +0200

    Sync VagAbfahrten-Init.js with current main

--- VagAbfahrten-Init.js
+++ VagAbfahrten-Init.js
@@ -4,7 +4,7 @@
 // Copy this script to Scriptable and run it once. After a successful install
 // it removes itself from the Scriptable storage it was launched from.
 
-const APP_VERSION = '1.0.7';
+const APP_VERSION = '1.0.9';
 
 const FILES = [
   ['VagAbfahrten.js', ["const APP_VERSION = '", "const TRIAS_ENDPOINT = 'https://efa-bw.de/trias';", 'await main();']],
